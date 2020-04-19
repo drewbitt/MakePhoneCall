d670e5a3849cc6932acb5bdace3c9280cd822953
drewbitt <andrew@drewbitt.com>
2020-04-19 17:51:56 -0400

    Working

--- main.py
+++ main.py
@@ -3,18 +3,9 @@ from twilio.rest import Client
 import configparser
 import argparse
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Make a call from audio using Twilio')
-    parser.add_argument('--audio_url', help='Url to audio file', required=True)
-    parser.add_argument('--to', help='Phone number to call', required=True)
-    parser.add_argument('--from', help='Phone number the call comes from', required=True)
-
-    args = parser.parse_args()
-
-    main(args['audio_url'], args['to'], args['from'])
-
 def main(audio_url, toNumber, fromNumber):
     config = configparser.ConfigParser()
+    config.read('config.ini')
 
     # Your Account Sid and Auth Token from twilio.com/console
     # DANGER! This is insecure. See http://twil.io/secure
@@ -24,10 +15,20 @@ def main(audio_url, toNumber, fromNumber):
 
     call = client.calls.create(
                             twiml='''<Response>
-    <Play></Play>
-    </Response>''',
+    <Play>{}</Play>
+    </Response>'''.format(audio_url),
                             to=toNumber,
                             from_=fromNumber
                         )
 
     print(call.sid)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Make a call from audio using Twilio')
+    parser.add_argument('--audio_url', help='Url to audio file', required=True)
+    parser.add_argument('--to', help='Phone number to call', required=True)
+    parser.add_argument('--from', dest='fromNumber', help='Phone number the call comes from', required=True)
+
+    args = parser.parse_args()
+
+    main(args.audio_url, args.to, args.fromNumber)
