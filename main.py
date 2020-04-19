0f7570b72ce46bd29b3562c77d0e21d97fe626a4
drewbitt <andrew@drewbitt.com>
2020-04-19 17:42:20 -0400

    Initial

---
+++ main.py
@@ -0,0 +1,33 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+import configparser
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Make a call from audio using Twilio')
+    parser.add_argument('--audio_url', help='Url to audio file', required=True)
+    parser.add_argument('--to', help='Phone number to call', required=True)
+    parser.add_argument('--from', help='Phone number the call comes from', required=True)
+
+    args = parser.parse_args()
+
+    main(args['audio_url'], args['to'], args['from'])
+
+def main(audio_url, toNumber, fromNumber):
+    config = configparser.ConfigParser()
+
+    # Your Account Sid and Auth Token from twilio.com/console
+    # DANGER! This is insecure. See http://twil.io/secure
+    account_sid = config['DEFAULT']['account_sid']
+    auth_token = config['DEFAULT']['auth_token']
+    client = Client(account_sid, auth_token)
+
+    call = client.calls.create(
+                            twiml='''<Response>
+    <Play></Play>
+    </Response>''',
+                            to=toNumber,
+                            from_=fromNumber
+                        )
+
+    print(call.sid)
